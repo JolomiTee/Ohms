60492d9ecb04e0357878597fe6ae6cec84b0457c
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-03-06 09:50:54 +0100

    Card component done

--- src/components/Apartments.jsx
+++ src/components/Apartments.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Search from './Search'
+import Cards from './Cards'
 
 const Apartments = () => {
   return (
     <section className='apartments'>
       <Search />
+
+      <h2 className='apartmentHeader'>Apartments around you</h2>
+
+      <div className='cards'>
+        <Cards imgsrc='apt1.png' location='Kado Estate, Abuja' rooms={4} miles={100} price={130000} />
+      </div>
     </section>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import aptType from '../assets/images/flatDescriptionIcon.svg'
+import distance from '../assets/images/routing.svg'
+import Button from './Button'
 
-const Cards = () => {
+const Cards = (props) => {
   return (
-    <div>Cards</div>
+    <div className='card'>
+
+      <div className="cardImageContainer">
+
+        <img src={`../../src/assets/images/${props.imgsrc}`} alt="" className='cardImage' />
+
+      </div>
+
+      <div className="cardDetails">
+
+        <p className='aptLocation'>{props.location}</p>
+
+        <div className='metaInfoContainer'>
+
+          <div className="apartmentMeta">
+            <img src={aptType} alt="Apartment Icon" />
+            {props.rooms} bedroom flat
+          </div>
+
+          <div className="apartmentMeta">
+            <img src={distance} alt="Apartment Icon" />
+            {props.miles} miles from you
+          </div>
+
+        </div>
+
+        <div className='metaCta'>
+
+          <p className='aptPrice'>N {props.price}</p>
+          <Button className='primarybutton' text='View' />
+
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
